9f776e19a6487ddb0f3cafc336829b8eae29b431
CicerotheTraveller <duahe20@gmail.com>
2026-06-24 22:42:38 +0200

    Add mobile navigation toggle functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById("menuButton");
+const mobileNav = document.getElementById("mobileNav");
+
+menuButton.addEventListener("click", () => {
+  mobileNav.classList.toggle("open");
+});
